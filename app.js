4f0b3a983b711766625228176d8ed9d80b35772c
Camoes-7 <82127829+Camoes-7@users.noreply.github.com>
2024-09-07 18:05:56 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa");
+    let resultado = "";
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    console.log(campoPesquisa);
+
+  if(campoPesquisa == ""){
+    section.innerHTML = "<p>Nenhum resultado encontrado!</p>";
+    return;
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+  let titulo = "";
+  let descricao = "";
+
+    for(let estado of estados){
+      titulo = estado.titulo.toLowerCase();
+      descricao = estado.descricao.toLowerCase();
+
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || estado.link.includes(campoPesquisa)){
+        resultado += `
+            <div class="item-resultado">
+                <h2><a href="#" target="blank">${estado.titulo}</a></h2>
+                    <p class="descricao-meta">${estado.descricao}</p>
+                    <a href="${estado.link}" target="_blank">Saiba Mais!</a>
+                </div>
+`
+      }
+    section.innerHTML = resultado;
+}
+
+//verifica se o resultado foi encontrado
+  if (section.innerHTML = resultado){
+    return;
+  } else{
+    section.innerHTML = "<p>Nenhum resultado encontrado!</p>";
+    return;
+  }
+}
+
+
+/*
+function pesquisar() {
+  // 1. Obtém a seção HTML onde os resultados serão exibidos,
+  //    usando o ID "resultados-pesquisa".
+  let section = document.getElementById("resultados-pesquisa");
+
+  // 2. Imprime no console o elemento da seção para fins de depuração.
+  console.log(section);
+
+  // 3. Inicializa uma string vazia para armazenar os resultados da pesquisa.
+  let resultado = "";
+
+  // 4. Itera sobre cada estado no array "estados".
+  for (let estado of estados) {
+    // 5. Concatena uma nova div com as informações do estado à string "resultado".
+    //    * estado.titulo: Título do estado.
+    //    * estado.descricao: Descrição do estado.
+    //    * estado.link: Link para mais informações.
+    resultado += `
+      <div class="item-resultado">
+        <h2><a href="#" target="blank">${estado.titulo}</a></h2>
+        <p class="descricao-meta">${estado.descricao}</p>
+        <a href="${estado.link}" target="_blank">Saiba Mais!</a>
+      </div>
+    `;
+  }
+
+  // 6. Substitui o conteúdo HTML da seção pelos resultados da pesquisa.
+  section.innerHTML = resultado;
+} 
+  */  
---
+++ dados.js
@@ -0,0 +1,44 @@
+let estados = [
+    {
+        titulo: "Roraima",
+        descricao: "Roraima é um estado brasileiro localizado na Região Norte, conhecido por ser o mais setentrional do país. Sua capital é Boa Vista, e suas fronteiras se estendem por Venezuela e Guiana",
+        link: "https://pt.wikipedia.org/wiki/Roraima",
+        tags : "norte",
+    },
+    {
+        titulo: "Tocantins",
+        descricao: "Tocantins é um estado brasileiro localizado na Região Norte, conhecido por suas belezas naturais e pela produção de grãos. Sua capital é Palmas.",
+        link: "https://pt.wikipedia.org/wiki/Tocantins",
+        tags: "",
+    },
+    {
+        titulo: "Rio Grande do Norte",
+        descricao: "O Rio Grande do Norte é um estado brasileiro localizado na Região Nordeste, famoso por suas praias paradisíacas e pelo polo tecnológico de Natal. Sua capital é Natal.",
+        link: "https://pt.wikipedia.org/wiki/Rio_Grande_do_Norte"
+    },
+    {
+        titulo: "Minas Gerais",
+        descricao: "Minas Gerais é um estado brasileiro localizado na Região Sudeste, conhecido por sua rica história, cultura e produção mineral. Sua capital é Belo Horizonte.",
+        link: "https://pt.wikipedia.org/wiki/Minas_Gerais"
+    },
+    {
+        titulo: "Bahia",
+        descricao: "A Bahia é um estado brasileiro localizado na Região Nordeste, conhecido por sua diversidade cultural, culinária rica e pelo Carnaval de Salvador. Sua capital é Salvador.",
+        link: "https://pt.wikipedia.org/wiki/Bahia"
+    },
+    {
+        titulo: "Rio de Janeiro",
+        descricao: "O Rio de Janeiro é um estado brasileiro localizado na Região Sudeste, famoso por suas paisagens exuberantes, como o Cristo Redentor e o Pão de Açúcar. Sua capital é Rio de Janeiro.",
+        link: "https://pt.wikipedia.org/wiki/Rio_de_Janeiro"
+    },
+    {
+        titulo: "Amazonas",
+        descricao: "O Amazonas é o maior estado brasileiro em extensão territorial e o que possui a maior biodiversidade do planeta. Sua capital é Manaus.",
+        link: "https://pt.wikipedia.org/wiki/Amazonas"
+    },
+    {
+        titulo: "São Paulo",
+        descricao: "São Paulo é o estado mais rico e populoso do Brasil, um importante centro industrial e financeiro. Sua capital é São Paulo.tetetetetetetetete",
+        link: "https://pt.wikipedia.org/wiki/São_Paulo"
+    }
+];
